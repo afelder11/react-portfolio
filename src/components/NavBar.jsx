2b7ca0f753576cced255536a6f95f36b47dcae19
Austin Felder <afellafelder@yahoo.com>
2024-09-08 17:43:46 -0400

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import logo from "../assets/personallogo.png";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+
+const NavBar = () => {
+    return (
+        <nav className="mb-20 flex items-center justify-between py-6">
+            <div className="flex flex-shrink-0 items-center">
+                <img className="mx-2 w-10" src={logo} alt="logo" />
+            </div>
+            <div className="m=8 flex items-center justify-center gap-4 text-2xl">
+                <a href="https://www.linkedin.com/in/austinfelder/" target="_blank" rel="noopener noreferrer">
+                    <FaLinkedin />
+                </a>
+                <a href="https://github.com/afelder11" target="_blank" rel="noopener noreferrer">
+                    <FaGithub />
+                </a>
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
---
+++ src/constants/index.js
@@ -0,0 +1,83 @@
+import project1 from "../assets/projects/project-1.png";
+import project2 from "../assets/projects/project-2.png";
+import project3 from "../assets/projects/project-3.png";
+import project4 from "../assets/projects/project-4.png";
+
+export const HERO_CONTENT = `I am a passionate application developer with a strong ability to craft robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies, particularly MySQL, T-SQL, and PHP. Additionally, I have proficiency in Python, Swift, and working with cloud platforms like Azure. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = 'I am a dedicated and versatile application developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies such as MySQL, T-SQL, Python, Swift, and PHP. Additionally, I have experience working with cloud platforms like Azure. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and playing video games.';
+
+export const EXPERIENCES = [
+  {
+    year: "Mar 2022 - Present",
+    role: "Jr Application's Developer",
+    company: "Los Angeles Pacific University",
+    description: `Responsible for providing technical support for all of the installed ERP applications as well as cloud-based applications. Provi-de development and support to customize, upgrade, and maintain applications.`,
+    technologies: ["PHP", "T-SQL", "C#", "RESTful APIs", "Azure", "Informatica"],
+  },
+  {
+    year: "Jan 2022 - Mar 2023",
+    role: "IT Intern",
+    company: "Wanzek Construction Inc.",
+    description: `Responsible for assisting with maintaining day-to-day computer operations of Wanzek Construction, in addition to long-term planning and development of computer operations.`,
+    technologies: ["Networking", "Microsoft Office Suite", "ServiceNow", "Windows OS", "Power BI"],
+  },
+  {
+    year: "Apr 2021 - Oct 2021",
+    role: "Technical Support Specialist",
+    company: "Teleperformance",
+    description: `Troubleshooted mobile iOS technology (iPhones & iPads), equipment and system problems while educating the customer on support options and steps being taken to resolve the issue`,
+    technologies: ["iOS", "CRM Software"],
+  },
+  {
+    year: "Aug 2020 - Apr 2021",
+    role: "Mobile App Developer Intern (Project Manager)",
+    company: "Moving Mountains Tutoring",
+    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
+    technologies: ["Swift", "Github", "RESTful APIs", "Firebase"],
+  },
+  {
+    year: "Aug 2020 - Apr 2021",
+    role: "Facilitator/Instructor",
+    company: "FIU CS-STEM Afterschool Program",
+    description: `Elucidated the students’ existing knowledge, prepared useful training material, and monitored the student’s performance.`,
+    technologies: ["Javascript", "Scratch Programming"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Mobile Game: SpaceWar Lite",
+    image: project1,
+    description:
+      "iOS shooter game that lets you command a spaceship and battle enemy spacecrafts in epic space combat!",
+    technologies: ["Swift", "XCode", "SpriteKit", "UIKit"],
+  },
+  {
+    title: "Cryptocurrency Tracker",
+    image: project2,
+    description:
+      "A real-time crypto tracker app that allows users to monitor cryptocurrency prices and market trends, providing detailed insights into their favorite coins.",
+    technologies: ["Swift", "RESTful APIs", "Firebase", "XCode"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Cryptocurrency Trading Bot",
+    image: project4,
+    description:
+      "An automated crypto trading bot that analyzes market data and executes trades based on predefined strategies, optimizing for real-time opportunities and maximizing potential returns.",
+    technologies: ["Python", "RESTful APIs", "Pandas", "CCXT"],
+  },
+];
+
+export const CONTACT = {
+  address: "767 Fifth Avenue, New York, NY 10153 ",
+  phoneNo: "(305) 898-3980",
+  email: "austincfelder@gmail.com",
+};
