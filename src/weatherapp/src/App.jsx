de08b75f24316a8769189c731ffb281216ad1720
Austin Felder <afellafelder@yahoo.com>
2024-09-25 22:47:39 -0400

    adding weather app routing

--- src/weatherapp/src/App.jsx
+++ src/weatherapp/src/App.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
-import Weather from './components/Weather'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Weather from './components/Weather';
+import Home from './components/Home'; // Optional: if you have a Home component
+import Navbar from './components/Navbar'; // Optional: if you have a Navbar component
 
 const App = () => {
   return (
-    <div className='app'>
-      <Weather/>
+    <Router>
+      <div className='app'>
+        {/* Optional Navbar for navigation */}
+        <Navbar />
 
-    </div>
-  )
+        {/* Define the routes */}
+        <Routes>
+          {/* Home route */}
+          <Route path="/" element={<Home />} />
+
+          {/* Weather app route */}
+          <Route path="/weatherapp" element={<Weather />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
