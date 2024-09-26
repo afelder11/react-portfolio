ea7a8bfe7ffac3e9c646c3d03c49073f5d2af2fb
Austin Felder <afellafelder@yahoo.com>
2024-09-25 22:58:45 -0400

    adding weather app routing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/profilepic.png"
+import profilePic from '../assets/profilepic.png';
 import { motion } from "framer-motion";
 
 const container = (delay) => ({
